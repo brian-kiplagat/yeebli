24dc1555676abc0199e78b19dae723245d0e1e07
Brian Kiplagat <kiplagatbrian18@gmail.com>
2025-02-22 12:57:15 +0300

    fix: versions

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { greetUser } from '$utils/greet';
 import { RouteGuard } from '$utils/routeGuards';
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'Brian Kiplagat';
+  const name = 'Brian Test Kiplagat';
   greetUser(name);
   RouteGuard.checkAccess();
 });
